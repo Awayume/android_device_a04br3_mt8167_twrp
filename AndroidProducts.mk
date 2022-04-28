dfd8f5340475a0b365031964e0dcc9f9f771aff8
yuu <46545607+mouseos@users.noreply.github.com>
2022-04-28 15:36:21 +0900

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/a04br3.mk
---
+++ a04br3.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/sts-tottori/a04br3/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := a04br3
+PRODUCT_NAME := a04br3
+PRODUCT_BRAND := benesse
+PRODUCT_MANUFACTURER := sts-tottori
+PRODUCT_MODEL := a04br3
